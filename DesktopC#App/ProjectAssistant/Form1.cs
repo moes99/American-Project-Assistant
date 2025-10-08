14ca7d721eb1aeee8cf1daeff921b10bb8d88758
Moe_Work <Momar@escomint.com>
2025-10-08 13:53:40 +0300

    Trying committing changes from vs

--- DesktopC#App/ProjectAssistant/Form1.cs
+++ DesktopC#App/ProjectAssistant/Form1.cs
@@ -2,9 +2,17 @@ namespace ProjectAssistant
 {
     public partial class Form1 : Form
     {
+
+        private readonly Random _rand = new Random();
+        int r, g, b;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1Click(object sender, EventArgs e)
+        {
+            button1.BackColor = System.Drawing.Color.FromArgb(r = _rand.Next(256), g = _rand.Next(256), b = _rand.Next(256));
         }
     }
 }
