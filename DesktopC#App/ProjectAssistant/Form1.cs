1e77d4b85847fac8267a375b6695a6d433286d51
moes99 <mohammadomarshehab@gmail.com>
2025-12-31 23:09:20 +0200

    info pane ui done. missing some logic

--- DesktopC#App/ProjectAssistant/Form1.cs
+++ DesktopC#App/ProjectAssistant/Form1.cs
@@ -1,6 +1,9 @@
 namespace ProjectAssistant
 {
     using FontAwesome.Sharp;
+    using System.IO;
+    using System.Windows.Forms;
+
     public partial class mainForm : Form
     {
         TableLayoutPanel currentPane = new TableLayoutPanel();
@@ -8,7 +11,7 @@ namespace ProjectAssistant
         {
             InitializeComponent();
             currentPane = infoPane;
-            infoButton.Focus();
+            this.Shown += mainForm_Shown;
         }
 
         // Method to change the visible panel
@@ -29,6 +32,8 @@ namespace ProjectAssistant
                 projectPathTBox.Text = "\\\\192.168.1.224\\New8TB\\1-Projects In Hand";
                 projectPathTBox.Enabled = false;
                 folderPathLabel.Text = "Parent Path:";
+                setPathButton.Enabled = false;
+                projectInfoBox.Enabled = true;
             }
         }
 
@@ -38,12 +43,88 @@ namespace ProjectAssistant
             {
                 projectPathTBox.Text = "";
                 projectPathTBox.Enabled = true;
+                setPathButton.Enabled = true;
+                folderPathLabel.Text = "Project Path:";
+                projectInfoBox.Enabled = false;
             }
         }
 
         private void infoButton_Click(object sender, EventArgs e)
         {
             changePanel(infoPane);
+        }
+
+        private void projectAddressTable_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        // Shown event handler: focus the infoButton after the form is displayed
+        private void mainForm_Shown(object sender, System.EventArgs e)
+        {
+            this.Shown -= mainForm_Shown;
+            this.BeginInvoke((System.Action)(() => infoButton.Focus()));
+        }
+
+        private void setPathButton_Click(object sender, EventArgs e)
+        {
+            if (projectPathTBox.Text != "")
+            {
+                if (Directory.Exists(projectPathTBox.Text))
+                {
+                    setPathButton.BackColor = System.Drawing.Color.LightGreen;
+                    projectInfoBox.Enabled = true;
+                    MessageBox.Show("Path has been set successfully.", "Valid Path", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    setPathButton.BackColor = System.Drawing.Color.PaleVioletRed;
+                    projectInfoBox.Enabled = false;
+                    MessageBox.Show("The specified folder does not exist! Please enter a valid path.", "Invalid Path", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void scopeList_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            string itemText = scopeList.Items[e.Index].ToString();
+            switch (itemText)
+            {
+                case "Mechanical":
+                    mechDevTBox.Enabled = e.NewValue == CheckState.Checked;
+                    mechDevLabel.Enabled = e.NewValue == CheckState.Checked;
+                    break;
+
+                case "Electrical":
+                    elecDevTBox.Enabled = e.NewValue == CheckState.Checked;
+                    elecDevLabel.Enabled = e.NewValue == CheckState.Checked;
+                    break;
+
+                case "Plumbing":
+                    plumbDevTBox.Enabled = e.NewValue == CheckState.Checked;
+                    plumbDevLabel.Enabled = e.NewValue == CheckState.Checked;
+                    break;
+
+                case "Fire Protection":
+                    fireDevTBox.Enabled = e.NewValue == CheckState.Checked;
+                    fireDevLabel.Enabled = e.NewValue == CheckState.Checked;
+                    break;
+
+                case "T24/Energy":
+                    engDevTBox.Enabled = e.NewValue == CheckState.Checked;
+                    engDevLabel.Enabled = e.NewValue == CheckState.Checked;
+                    break;
+
+                case "Structure":
+                    structDevTBox.Enabled = e.NewValue == CheckState.Checked;
+                    structDevLabel.Enabled = e.NewValue == CheckState.Checked;
+                    break;
+
+                case "Architecture":
+                    archDevTBox.Enabled = e.NewValue == CheckState.Checked;
+                    archDevLabel.Enabled = e.NewValue == CheckState.Checked;
+                    break;
+            }
         }
     }
 }
